cf99c0eeebb8605e7340298472f513f0bc65d804
lgalstyan <larisagalstya@gmail.com>
2023-03-03 00:11:49 +0400

    gets message but not parsed yet

--- server.c
+++ server.c
@@ -48,70 +48,66 @@ char *ft_exec(char *buff, char **env)
 	if (pid == 0)
 	{
 		child_proc(buff, env);
-	}
-
+    }
     return (buff);
 }
 
-int main(int argc, char **argv, char **env)
+
+int main(int argc , char **argv, char **env)
 {
     (void)argc;
     (void)argv;
-    char *ip = "127.0.0.1";
-    char **token;
-
-    int server_sock, client_sock;
-    struct sockaddr_in server_addr, client_addr;
-    socklen_t addr_size;
-    char *buffer;
-    int n;
-
-    buffer = NULL;
-    server_sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_sock < 0)
-    {
-        perror("Socket error\n");
-        exit(1);
-    }
-    printf("TCP server socket created.\n");
-    memset(&server_addr, '\0', sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = PORT;
-    server_addr.sin_addr.s_addr = inet_addr(ip);
-    n = bind(server_sock, (struct sockaddr*)&server_addr, sizeof(server_addr));
-    if (n < 0){
-        perror("Bind error\n");
-        exit(1);
-    }
-    printf("Bind to the port number: %d\n", PORT);
-    listen(server_sock, 5);
-    printf("Listening...\n");
-    while(1)
-    {
-        addr_size = sizeof(client_addr);
-        client_sock = accept(server_sock, (struct sockaddr*)&client_addr, &addr_size);
-        printf("Client connected.\n");
-        bzero(buffer, 1024);
-        recv(client_sock, buffer, sizeof(buffer), 0);
-        token = ft_split(buffer, ' ');
-        printf("recv\n");
-        bzero(buffer, 1024);
-        if (token != NULL && token[0] != NULL && !ft_strcmp(buffer, "disconnect"))
-        {
-            send(client_sock, "disconnect", 10, 0);
-            close(client_sock);
-            printf("Client disconnected.\n\n");
-        }
-        else if (token != NULL && token[0] != NULL && !ft_strcmp(buffer, "shell"))
-        {
-            buffer = ft_exec(strtok(token[1], "\""), env);
-            // strcpy(buffer, strlen(buffer));
-            // strcpy(buffer, ft_exec(strtok(tokne[1], "\""), env));
-            send(client_sock, buffer, strlen(buffer), 0);
-        }
-        else
-            send(client_sock, buffer, strlen(buffer), 0);
-    }
-
-    return 0;
+	int socket_desc , client_sock , c , read_size;
+	struct sockaddr_in server , client;
+	char client_message[2000];
+	
+	socket_desc = socket(AF_INET , SOCK_STREAM , 0);
+	if (socket_desc == -1)
+	{
+		printf("Could not create socket");
+	}
+	puts("Socket created");
+	
+	server.sin_family = AF_INET;
+	server.sin_addr.s_addr = INADDR_ANY;
+	server.sin_port = htons( 8888 );
+	
+	if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0)
+	{
+		perror("bind failed. Error");
+		return 1;
+	}
+	puts("bind done");
+	
+	listen(socket_desc , 5);
+	
+	puts("Waiting for incoming connections...");
+	c = sizeof(struct sockaddr_in);
+	
+	client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c);
+	if (client_sock < 0)
+	{
+		perror("accept failed");
+		return 1;
+	}
+	puts("Connection accepted");
+	
+	while( (read_size = recv(client_sock , client_message , 2000 , 0)) > 0 )
+	{
+		write(client_sock , client_message , strlen(client_message));
+	    if(read_size == 0 || !ft_strcmp(client_message, "disconnect"))
+	    {
+	    	puts("Client disconnected");
+	    	fflush(stdout);
+	    }
+		write(client_sock , ft_exec(client_message, env) , strlen(client_message));
+        bzero(client_message, strlen(client_message));
+	}
+	
+	if(read_size == -1)
+	{
+		perror("recv failed");
+	}
+	
+	return 0;
 }
