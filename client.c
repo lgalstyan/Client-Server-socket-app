0de1573740955780f385eaf814743d35b66e762f
lgalstyan <larisagalstya@gmail.com>
2023-03-04 22:46:00 +0400

    Need to undestand how read the message correncly with readline or scanf

--- client.c
+++ client.c
@@ -4,18 +4,24 @@ int main(int argc , char **argv)
 {
 	(void)argc;
 	int sock;
+	if (argc != 3)
+	{
+		printf("IP and port arguments should be\n");
+		return (1);
+	}
     const char *ip = argv[1];
     int port = atoi(argv[2]);
+	char *message = NULL;
 
 	struct sockaddr_in server;
-	char message[2000] , server_reply[2000];
+	// char message[2000] ;
+	char server_reply[2000];
 	
 	sock = socket(AF_INET , SOCK_STREAM , 0);
 	if (sock == -1)
 	{
 		printf("Could not create socket");
 	}
-	// printf("Socket created\n");
 	server.sin_addr.s_addr = inet_addr(ip);
 	server.sin_family = AF_INET;
 	server.sin_port = htons(port);
@@ -25,12 +31,15 @@ int main(int argc , char **argv)
 		perror("connect failed. Error");
 		return 1;
 	}
-	// printf("Connected\n");
 	while(1)
 	{
-		printf(ESC_GREEN"Client> "ESC_WHITE);
-		bzero(message, strlen(message));
-		scanf("%s" , message);
+		// printf(ESC_GREEN"Client> "ESC_WHITE);
+		// bzero(message, strlen(message));
+		// message = readline(ESC_GREEN"Client> "ESC_WHITE);
+		message = readline(">");
+		// scanf("%s" , message);
+		// puts("hello");
+		// puts(message);
 		
 		if( send(sock , message , strlen(message) , 0) < 0)
 		{
@@ -43,106 +52,17 @@ int main(int argc , char **argv)
 			printf("recv failed\n");
 			break;
 		}
-		printf("serv_repl is %s\n", server_reply);
+		// printf("serv_repl is %s\n", server_reply);
 		if (!strncmp(server_reply, "disconnect", 10))
 		{
-			printf("eeey Client disconnected\n");
+			printf("Client disconnected\n");
+			// fflush(stdout);
 			// close(sock);
-			fflush(stdout);
 			break;
 		}
-		// printf("%s\n", server_reply);
+		free(message);
+		message = NULL;
 	}
-	close(sock);
-	return 0;
+	// close(sock);
+	return (0);
 }
-
-
-// int main(){
-
-//     char *ip = "127.0.0.1";
-//     int port = 5566;
-//     char *line; 
-//     int sock;
-//     int client_sock;
-//     struct sockaddr_in addr;
-//     char buffer[1024];  
-//     line = NULL;
-//     sock = socket(AF_INET, SOCK_STREAM, 0);
-//     if (sock < 0){
-//       perror("Socket error");
-//       exit(1);
-//     }
-//     memset(&addr, '\0', sizeof(addr));
-//     addr.sin_family = AF_INET;
-//     addr.sin_port = port;
-//     addr.sin_addr.s_addr = inet_addr(ip);   
-//     connect(sock, (struct sockaddr*)&addr, sizeof(addr));
-//     printf("Connected to the server.\n");   
-//     bzero(buffer, 1024);
-//     line = readline("Client> ");
-//     strcpy(buffer, line);
-//     printf("Client: %s\n", buffer);
-//     client_sock = send(sock, buffer, strlen(buffer), 0);
-//     if (client_sock < 0)
-//     {
-// 	    perror("accept failed");
-// 	    return (3);
-//     }
-//     printf("Server: %s\n", buffer);
-
-//     bzero(buffer, 1024);
-//     recv(sock, buffer, sizeof(buffer), 0);
-
-// //   close(sock);
-// //   printf("Disconnected from the server.\n");
-//     return 0;
-// }
-
-
-
-// int main(int argc, char **argv)
-// {
-//     if (argc != 3)
-//         return (2);
-//     char *ip = argv[1];
-//     int port = atoi(argv[2]);
-//     int sock;
-//     struct sockaddr_in addr;
-    
-//     char *line;
-//     line = NULL;
-//     sock = socket(AF_INET, SOCK_STREAM, 0);
-//     if (sock < 0)
-//     {
-//         perror("Socket error");
-//         exit(1);
-//     }
-//     memset(&addr, '\0', sizeof(addr));
-//     addr.sin_family = AF_INET;
-//     addr.sin_port = port;
-//     addr.sin_addr.s_addr = inet_addr(ip);
-
-//     while (1)
-//     {
-//         connect(sock, (struct sockaddr*)&addr, sizeof(addr));
-//         bzero(line, 1024);
-// 	    line = readline("💚 client> "ESC_WHITE);
-// 	    if (!line)
-// 	    	continue ;
-//         printf("Client: %s\n", line);
-//         send(sock, line, strlen(line), 0);
-//         bzero(line, 1024);
-//         recv(sock, line, sizeof(line), 0);
-//         printf("Server: %s\n", line);
-//         if (!ft_strcmp(line, "disconnect"))
-//         {
-//             close(sock);
-//             printf("Disconnected from the server.\n");
-//             return (2);
-//         }
-//         free(line);
-//     }
-//     return (0);
-
-// }
