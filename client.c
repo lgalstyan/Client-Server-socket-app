2d258cc71dc3220af61ae2904efdf4e80cab9832
lgalstyan <larisagalstya@gmail.com>
2023-03-05 00:46:51 +0400

    Works normally. Some tests still need to be done

--- client.c
+++ client.c
@@ -3,19 +3,17 @@
 int main(int argc , char **argv)
 {
 	(void)argc;
-	int sock;
 	if (argc != 3)
 	{
 		printf("IP and port arguments should be\n");
 		return (1);
 	}
+	int sock;
     const char *ip = argv[1];
     int port = atoi(argv[2]);
 	char *message = NULL;
-
-	struct sockaddr_in server;
-	// char message[2000] ;
 	char server_reply[2000];
+	struct sockaddr_in server;
 	
 	sock = socket(AF_INET , SOCK_STREAM , 0);
 	if (sock == -1)
@@ -29,22 +27,17 @@ int main(int argc , char **argv)
 	if (connect(sock, (struct sockaddr *)&server , sizeof(server)) < 0)
 	{
 		perror("connect failed. Error");
-		return 1;
+		return (1);
 	}
 	while(1)
 	{
-		// printf(ESC_GREEN"Client> "ESC_WHITE);
-		// bzero(message, strlen(message));
-		// message = readline(ESC_GREEN"Client> "ESC_WHITE);
-		message = readline(">");
-		// scanf("%s" , message);
-		// puts("hello");
-		// puts(message);
-		
-		if( send(sock , message , strlen(message) , 0) < 0)
+		message = NULL;
+		message = readline(ESC_GREEN "Client> " ESC_WHITE);
+		add_history(message);
+		if( send(sock, message, strlen(message), 0) < 0)
 		{
 			printf("Send failed\n");
-			return 1;
+			return (1);
 		}
 		bzero(server_reply, strlen(server_reply));
 		if( recv(sock , server_reply , 2000 , 0) < 0)
@@ -52,12 +45,10 @@ int main(int argc , char **argv)
 			printf("recv failed\n");
 			break;
 		}
-		// printf("serv_repl is %s\n", server_reply);
+		write(1, server_reply, strlen(server_reply));
 		if (!strncmp(server_reply, "disconnect", 10))
 		{
 			printf("Client disconnected\n");
-			// fflush(stdout);
-			// close(sock);
 			break;
 		}
 		free(message);
